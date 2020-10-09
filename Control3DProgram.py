79ded95e0383fd596e5fd7b28368ae3592d3e378
ragnarr18 <ragnarr18@skel.ru.is>
2020-10-09 16:19:06 +0000

    minimap ready

--- Control3DProgram.py
+++ Control3DProgram.py
@@ -174,6 +174,7 @@ class GraphicsProgram3D:
             glClearColor(0.0, 0.0, 0.0, 1.0)
             # glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
             # glViewport(500, 0, 300, 300)
+            glClear(GL_DEPTH_BUFFER_BIT)
             glViewport(500, 0, 300, 300)
 
             # self.projection_matrix.set_perspective(self.fov , 888/ 600, 0.05, 100000)
